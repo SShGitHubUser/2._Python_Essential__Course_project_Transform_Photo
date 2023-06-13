eb1ce7101a33935cec0c8440f212188547e531d2
COMMON-PC\Common <shypulin@ukr.net>
2023-06-13 23:38:54 +0300

    Добавлены действия на все кнопки

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class MainWindow(QMainWindow):
                                                    lambda: self.transform_image('Edge enhance more'), '')
         act_smooth = self.create_action('Smooth', lambda: self.transform_image('Smooth'), '')
         act_smooth_more = self.create_action('Smooth more', lambda: self.transform_image('Smooth more'), '')
-        act_b_w = self.create_action('B/W', lambda: self.transform_image('B_W'), '')
+        act_b_w = self.create_action('B/W', lambda: self.transform_image('B/W'), '')
         act_contour = self.create_action('Contour', lambda: self.transform_image('Contour'), '')
         act_flip = self.create_action('Flip', lambda: self.transform_image('Flip'), '')
         act_emboss = self.create_action('Emboss', lambda: self.transform_image('Emboss'), '')
@@ -63,50 +63,23 @@ class MainWindow(QMainWindow):
         self.setMenuBar(menu_main)
         # создаем меню Файл
         menu_file = QMenu('Файл', self)
-        menu_file.addAction(act_open)
-        menu_file.addAction(act_save)
-        menu_file.addAction(act_exit)
+        menu_file.addActions([act_open, act_save, act_exit])
         # создаем меню Преобразовать
         menu_edit = QMenu('Преобразовать', self)
-        menu_edit.addAction(act_left)
-        menu_edit.addAction(act_right)
-        menu_edit.addAction(act_sharp)
-        menu_edit.addAction(act_blur)
-        menu_edit.addAction(act_edge_enhance)
-        menu_edit.addAction(act_edge_enhance_more)
-        menu_edit.addAction(act_smooth)
-        menu_edit.addAction(act_smooth_more)
-        menu_edit.addAction(act_b_w)
-        menu_edit.addAction(act_contour)
-        menu_edit.addAction(act_flip)
-        menu_edit.addAction(act_emboss)
-        menu_edit.addAction(act_gaussian_blur)
-        menu_edit.addAction(act_unsharp_mask)
-        menu_edit.addAction(act_detail)
-        menu_edit.addAction(act_find_edges)
+        menu_edit.addActions(
+            [act_left, act_right, act_sharp, act_blur, act_edge_enhance, act_edge_enhance_more, act_smooth,
+             act_smooth_more, act_b_w, act_contour, act_flip, act_emboss, act_gaussian_blur, act_unsharp_mask,
+             act_detail, act_find_edges])
         # добавляем все подменю в главное меню
         menu_main.addMenu(menu_file)
         menu_main.addMenu(menu_edit)
         menu_main.addAction(act_about)
 
         # Добавляем контекстное меню
-        self.context_menu.addAction(act_left)
-        self.context_menu.addAction(act_left)
-        self.context_menu.addAction(act_right)
-        self.context_menu.addAction(act_sharp)
-        self.context_menu.addAction(act_blur)
-        self.context_menu.addAction(act_edge_enhance)
-        self.context_menu.addAction(act_edge_enhance_more)
-        self.context_menu.addAction(act_smooth)
-        self.context_menu.addAction(act_smooth_more)
-        self.context_menu.addAction(act_b_w)
-        self.context_menu.addAction(act_contour)
-        self.context_menu.addAction(act_flip)
-        self.context_menu.addAction(act_emboss)
-        self.context_menu.addAction(act_gaussian_blur)
-        self.context_menu.addAction(act_unsharp_mask)
-        self.context_menu.addAction(act_detail)
-        self.context_menu.addAction(act_find_edges)
+        self.context_menu.addActions(
+            [act_left, act_right, act_sharp, act_blur, act_edge_enhance, act_edge_enhance_more, act_smooth,
+             act_smooth_more, act_b_w, act_contour, act_flip, act_emboss, act_gaussian_blur, act_unsharp_mask,
+             act_detail, act_find_edges])
         # активируем контекстное меню
         self.lbl_image.setContextMenuPolicy(Qt.CustomContextMenu)
         self.lbl_image.customContextMenuRequested.connect(self.show_context_menu)
@@ -122,25 +95,24 @@ class MainWindow(QMainWindow):
 
         # Создаем макет правой части - для выбора отображения выбранного файла и кнопок преобразования
 
-        # Создаем кнопки для преобразования изображения и добавляем их в макет для них
-        btn_left = QPushButton(act_left.text())
-        btn_left.addAction(act_left)
+        # Создаем кнопки для преобразования изображения и добавляем их в соответствующий макет
+        btn_left = self.create_button(act_left)
+        btn_right = self.create_button(act_right)
+        btn_sharp = self.create_button(act_sharp)
+        btn_blur = self.create_button(act_blur)
+        btn_edge_enhance = self.create_button(act_edge_enhance)
+        btn_edge_enhance_more = self.create_button(act_edge_enhance_more)
+        btn_smooth = self.create_button(act_smooth)
+        btn_smooth_more = self.create_button(act_smooth_more)
+        btn_b_w = self.create_button(act_b_w)
+        btn_contour = self.create_button(act_contour)
+        btn_flip = self.create_button(act_flip)
+        btn_emboss = self.create_button(act_emboss)
+        btn_gaussian_blur = self.create_button(act_gaussian_blur)
+        btn_unsharp_mask = self.create_button(act_unsharp_mask)
+        btn_detail = self.create_button(act_detail)
+        btn_find_edges = self.create_button(act_find_edges)
 
-        btn_right = QPushButton('Right')
-        btn_sharp = QPushButton('Sharp')
-        btn_blur = QPushButton('Blur')
-        btn_edge_enhance = QPushButton('Edge enhance')
-        btn_edge_enhance_more = QPushButton('Edge enhance more')
-        btn_smooth = QPushButton('Smooth')
-        btn_smooth_more = QPushButton('Smooth more')
-        btn_b_w = QPushButton('B/W')
-        btn_contour = QPushButton('Contour')
-        btn_flip = QPushButton('Flip')
-        btn_emboss = QPushButton('Emboss')
-        btn_gaussian_blur = QPushButton('Gaussian Blur')
-        btn_unsharp_mask = QPushButton('Unsharp Mask')
-        btn_detail = QPushButton('Detail')
-        btn_find_edges = QPushButton('Find edges')
         lo_transform = QGridLayout()
         lo_transform.addWidget(btn_left, 0, 0)
         lo_transform.addWidget(btn_right, 1, 0)
@@ -179,6 +151,13 @@ class MainWindow(QMainWindow):
         if shortcut:
             action.setShortcut(shortcut)
         return action
+
+    @staticmethod
+    def create_button(action: QAction) -> QPushButton:
+        button = QPushButton(action.text())
+        button.addAction(action)
+        button.clicked.connect(action.trigger)
+        return button
 
     def show_context_menu(self, position):
         global_position = self.lbl_image.mapToGlobal(position)
@@ -221,126 +200,35 @@ class MainWindow(QMainWindow):
         if command == 'Right':
             self.image = self.image.transpose(Image.ROTATE_270)
         if command == 'Sharp':
-            self.image = self.image.filter(Image.SHARPEN)
+            self.image = self.image.filter(SHARPEN)
         if command == 'Blur':
-            self.image = self.image.filter(Image.BLUR)
+            self.image = self.image.filter(BLUR)
         if command == 'Edge enhance':
-            self.image = self.image.filter(Image.EDGE_ENHANCE)
+            self.image = self.image.filter(EDGE_ENHANCE)
         if command == 'Edge enhance more':
-            self.image = self.image.filter(Image.EDGE_ENHANCE_MORE)
-        elif command == 'contour':
-            self.image = self.image.filter(ImageFilter.CONTOUR)
-        elif command == 'detail':
-            self.image = self.image.filter(ImageFilter.DETAIL)
-        elif command == 'edge_enhance':
-            self.image = self.image.filter(ImageFilter.EDGE_ENHANCE)
-        elif command == 'edge_enhance_more':
-            self.image = self.image.filter(ImageFilter.EDGE_ENHANCE_MORE)
-        elif command == 'emboss':
-            self.image = self.image.filter(ImageFilter.EMBOSS)
-        elif command == 'find_edges':
-            self.image = self.image.filter(ImageFilter.FIND_EDGES)
-        elif command == 'gaussian_blur':
-            self.image = self.image.filter(ImageFilter.GaussianBlur)
+            self.image = self.image.filter(EDGE_ENHANCE_MORE)
+        if command == 'Smooth':
+            self.image = self.image.filter(SMOOTH)
+        if command == 'Smooth more':
+            self.image = self.image.filter(SMOOTH_MORE)
+        if command == 'B/W':
+            self.image = self.image.convert("L")
+        if command == 'Contour':
+            self.image = self.image.filter(CONTOUR)
+        if command == 'Flip':
+            self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
+        if command == 'Emboss':
+            self.image = self.image.filter(EMBOSS)
+        if command == 'Gaussian blur':
+            self.image = self.image.filter(GaussianBlur)
+        if command == 'Unsharp mask':
+            self.image = self.image.filter(UnsharpMask)
+        if command == 'Detail':
+            self.image = self.image.filter(DETAIL)
+        if command == 'Find edges':
+            self.image = self.image.filter(FIND_EDGES)
         self.save_image()
         pass
-
-    act_smooth = self.create_action('Smooth', lambda: self.transform_image('Smooth'), '')
-    act_smooth_more = self.create_action('Smooth more', lambda: self.transform_image('Smooth more'), '')
-    act_b_w = self.create_action('B/W', lambda: self.transform_image('B_W'), '')
-    act_contour = self.create_action('Contour', lambda: self.transform_image('Contour'), '')
-    act_flip = self.create_action('Flip', lambda: self.transform_image('Flip'), '')
-    act_emboss = self.create_action('Emboss', lambda: self.transform_image('Emboss'), '')
-    act_gaussian_blur = self.create_action('Gaussian blur', lambda: self.transform_image('Gaussian blur'), '')
-    act_unsharp_mask = self.create_action('Unsharp mask', lambda: self.transform_image('Unsharp mask'), '')
-    act_detail = self.create_action('Detail', lambda: self.transform_image('Detail'), '')
-    act_find_edges = self.create_action('Find edges', lambda: self.transform_image('Find edges'), '')
-
-    #
-    #
-    #     def do_b_w(self):
-    #         self.image = self.image.convert("L")
-    #         self.save_image()
-    #         self.show_image(os.path.join(workdir, SAVE_DIR, self.filename))
-    #
-    #         """self.save_image()
-    #         image_path = os.path.join(workdir, self.save_dir, self.filename)
-    #         self.show_image(image_path)"""
-    #
-    #
-    #     def do_flip(self):
-    #         self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
-    #         """self.save_image()
-    #         image_path = os.path.join(workdir, self.save_dir, self.filename)
-    #         self.show_image(image_path)"""
-    #         self.save_image()
-    #         self.show_image(os.path.join(workdir, SAVE_DIR, self.filename))
-    #
-    #
-    #     def do_contour(self):
-    #         self.image = self.image.filter(CONTOUR)
-    #         """self.save_image()
-    #         image_path = os.path.join(workdir, self.save_dir, self.filename)
-    #         self.show_image(image_path)"""
-    #         self.save_image()
-    #         self.show_image(os.path.join(workdir, SAVE_DIR, self.filename))
-    #
-    #     def do_detail(self):
-    #         self.image = self.image.filter(DETAIL)
-    #         """self.save_image()
-    #         image_path = os.path.join(workdir, self.save_dir, self.filename)
-    #         self.show_image(image_path)"""
-    #         self.save_image()
-    #         self.show_image(os.path.join(workdir, SAVE_DIR, self.filename))
-    #
-    #
-    #     def do_emboss(self):
-    #         self.image = self.image.filter(EMBOSS)
-    #         """self.save_image()
-    #         image_path = os.path.join(workdir, self.save_dir, self.filename)
-    #         self.show_image(image_path)"""
-    #         self.save_image()
-    #         self.show_image(os.path.join(workdir, SAVE_DIR, self.filename))
-    #
-    #     def do_find_edges(self):
-    #         self.image = self.image.filter(FIND_EDGES)
-    #         """self.save_image()
-    #         image_path = os.path.join(workdir, self.save_dir, self.filename)
-    #         self.show_image(image_path)"""
-    #         self.save_image()
-    #         self.show_image(os.path.join(workdir, SAVE_DIR, self.filename))
-    #
-    #     def do_smooth(self):
-    #         self.image = self.image.filter(SMOOTH)
-    #         """self.save_image()
-    #         image_path = os.path.join(workdir, self.save_dir, self.filename)
-    #         self.show_image(image_path)"""
-    #         self.save_image()
-    #         self.show_image(os.path.join(workdir, SAVE_DIR, self.filename))
-    #
-    #     def do_smooth_more(self):
-    #         self.image = self.image.filter(SMOOTH_MORE)
-    #         """self.save_image()
-    #         image_path = os.path.join(workdir, self.save_dir, self.filename)
-    #         self.show_image(image_path)"""
-    #         self.save_image()
-    #         self.show_image(os.path.join(workdir, SAVE_DIR, self.filename))
-    #
-    #     def do_gaussian_blur(self):
-    #         self.image = self.image.filter(GaussianBlur)
-    #         """self.save_image()
-    #         image_path = os.path.join(workdir, self.save_dir, self.filename)
-    #         self.show_image(image_path)"""
-    #         self.save_image()
-    #         self.show_image(os.path.join(workdir, SAVE_DIR, self.filename))
-    #
-    #     def do_unsharp_mask(self):
-    #         self.image = self.image.filter(UnsharpMask)
-    #         """self.save_image()
-    #         image_path = os.path.join(workdir, self.save_dir, self.filename)
-    #         self.show_image(image_path)"""
-    #         self.save_image()
-    #         self.show_image(os.path.join(workdir, SAVE_DIR, self.filename))
 
     def show_info(self):
         self.prog_info = QMessageBox()
